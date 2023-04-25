fa0dadffba4709dacb97fd2185f139ee0f52ff09
samie54 <126026530+samie54@users.noreply.github.com>
2023-04-25 15:14:00 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,18 +1,17 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
 #include <stdio.h>
-
 #include <stdarg.h>
-
 #include <unistd.h>
+#include <limits.h>
+#include <stdlibs.h>
 
 
 
 #define UNUSED(x) (void)(x)
 
-#define BUF_SIZE 1024
+#define BUFF_SIZE 1024
 
 
 
