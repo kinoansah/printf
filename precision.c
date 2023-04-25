8bbe8399412675b74ba923d1a23f5ff20e05c048
kinoansah <125514115+kinoansah@users.noreply.github.com>
2023-04-25 07:57:03 +0000

    Update precision.c

--- precision.c
+++ precision.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <ctype.h>
 
 /**
  * get_precision - Calculates the precision for printing
@@ -13,33 +14,27 @@ int get_precision(const char *format, int *i, va_list list)
     int curr_i = *i + 1;
     int precision = -1;
 
-    /* Check if there is a precision specifier */
     if (format[curr_i] != '.')
-        return precision;
+        return (precision);
 
     precision = 0;
+    curr_i++;
 
-    /* Parse precision value from format string */
-    for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+    if (isdigit(format[curr_i]))
     {
-        if (isdigit(format[curr_i]))
+        while (isdigit(format[curr_i]))
         {
             precision *= 10;
             precision += format[curr_i] - '0';
-        }
-        else if (format[curr_i] == '*')
-        {
             curr_i++;
-            precision = va_arg(list, int);
-            break;
         }
-        else
-        {
-            break;
-        }
+    }
+    else if (format[curr_i] == '*')
+    {
+        curr_i++;
+        precision = va_arg(list, int);
     }
 
     *i = curr_i - 1;
-
-    return precision;
+    return (precision);
 }
