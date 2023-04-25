a7dc89eee1077bb899aa068af98da78d45682647
kinoansah <125514115+kinoansah@users.noreply.github.com>
2023-04-25 07:51:38 +0000

    Update precision.c

--- precision.c
+++ precision.c
@@ -1,55 +1,44 @@
 #include "main.h"
 
 /**
-
-get_precision - Calculates the precision for printing
-
-@format: Formatted string in which to print the arguments
-
-@i: Pointer to the index of the current character in format
-
-@list: List of arguments to be printed
-
-Return: The precision, or -1 if there is no precision
-*/
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
+ */
 int get_precision(const char *format, int *i, va_list list)
 {
-int curr_i = *i + 1;
-int precision = -1;
+	int curr_i = *i + 1;
+	int precision = -1;
+	int hex_precision = 0;
 
-// Check if there is a precision specifier
-if (format[curr_i] != '.')
-return (precision);
+	/* Check if there is a precision specifier */
+	if (format[curr_i] != '.')
+		return precision;
 
-// Parse the precision
-precision = 0;
-curr_i++;
-if (format[curr_i] == '*')
-{
-precision = va_arg(list, int);
-curr_i++;
-}
-else
-{
-while (is_digit(format[curr_i]))
-{
-precision = precision * 10 + (format[curr_i] - '0');
-curr_i++;
-}
-}
+	precision = 0;
 
-*i = curr_i - 1;
-return (precision);
-}
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (isdigit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-/**
+	*i = curr_i - 1;
 
-is_digit - Checks if a character is a digit
-@c: Character to be checked
-Return: 1 if c is a digit, 0 otherwise
-*/
-int is_digit(char c)
-{
-return (c >= '0' && c <= '9');
+	return precision;
 }
 
