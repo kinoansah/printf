529cfa4471da616ae499a3172c275e08f34fdf46
kinoansah <125514115+kinoansah@users.noreply.github.com>
2023-04-25 17:17:10 +0000

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,26 +1,21 @@
-#include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - Printf function
- *
- * @format: format string
- *
- * Return: number of printed characters
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed_chars = 0, buff_ind = 0, flags, width, precision, size, i, printed;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(args, format);
+	va_start(list, format);
 
-	for (i = 0; format && format[i]; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
@@ -33,12 +28,12 @@ int _printf(const char *format, ...)
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, args);
-			precision = get_precision(format, &i, args);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
-			i++;
-			printed = handle_print(format, &i, args, buffer,
-				flags, width, precision, size);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					       flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -46,20 +41,8 @@ int _printf(const char *format, ...)
 	}
 
 	print_buffer(buffer, &buff_ind);
-	va_end(args);
+
+	va_end(list);
 
 	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exists
- *
- * @buffer: array of characters
- * @buff_ind: index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-	*buff_ind = 0;
 }
